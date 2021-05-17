236932f3980d558b8d01ab14ce5a50796e734487
giale1234 <phuonggiale13@gmail.com>
2021-05-17 15:05:35 +0700

    test realtime MQTT

--- lib/screen/mainhomepage.dart
+++ lib/screen/mainhomepage.dart
@@ -1,173 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:multidisproject_2/screen/loginpage.dart';
 import 'package:multidisproject_2/screen/homepage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class MainHomePage extends StatefulWidget {
-
   @override
-  _MainHomePageState createState() => _MainHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MainHomePageState extends State<MainHomePage> {
-  @override
+class _HomePageState extends State<MainHomePage> {
+  bool relay1pressed = false;
+  final databaseReferenceTest = FirebaseFirestore.instance.collection('noise_feed').doc('qMBoZX6uRcUfkN3eebhi');
+
+@override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar (
-        backgroundColor: Colors.orangeAccent,
-        title: Text ("Homepage"),
-      ),
-      body: Center (
-        // child: MaterialButton (
-        //   onPressed: ()  async {
-        //     await FirebaseAuth.instance.signOut();
-        //   },
-        //   child: Text("Sign out"),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row (
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.computer,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('ROOM 1'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.pie_chart,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('Light Intensity', textAlign: TextAlign.center),
-                        ],
-                      ),
-                    ),
-
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.pie_chart,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('Noise', textAlign: TextAlign.center),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.computer,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('ROOM 2'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.pie_chart,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('Light Intensity', textAlign: TextAlign.center),
-                        ],
-                      ),
-                    ),
-
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.pie_chart,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('Noise', textAlign: TextAlign.center),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.computer,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('ROOM 3'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.pie_chart,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('Light Intensity', textAlign: TextAlign.center),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.pie_chart,
-                            color: Colors.green,
-                            size: 80.0,
-                          ),
-                          Text('Noise', textAlign: TextAlign.center),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          )
-      ),
-    );
-  }
-}
+    var data =''  ;
+  return  StreamBuilder(
+    stream: FirebaseFirestore.instance.collection("noise_feed").snapshots(includeMetadataChanges: true),
+    builder: (BuildContext context,
+              AsyncSnapshot snapshot) {  
+                 
+                if (snapshot.connectionState == ConnectionState.active){
+                   print(snapshot.data.docs[0].data()['value']);
+                    data = snapshot.data.docs[0].data()['value'];
+                }
+          return Container(
+            child:Row(
+            children:[
+                Text("${data}"),
+            LiteRollingSwitch(
+                  value: relay1pressed,
+                  textOn: 'active',
+                  textOff: 'inactive',
+                  colorOn: Colors.deepOrange,
+                  colorOff: Colors.blueGrey,
+                  iconOn: Icons.lightbulb_outline,
+                  iconOff: Icons.power_settings_new,
+                  onChanged: (bool state) {
+                    state
+                        ? databaseReferenceTest
+                            .update({'value': '1'})
+                        : databaseReferenceTest
+                            .update({'value': '0'}) ;
+                    }    )
+              ]
+            )
+          );
+    }
+  );
+}}
